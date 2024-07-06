410471a163cb01c55475bdccef105cab76997160
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2024-07-06 15:16:20 +0200

    container fix

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -9,7 +9,7 @@ export const Layout = ({ children }: LayoutChildrenProps ) => {
   return (
     <div className="h-screen flex flex-col justify-between">
       <Header/>
-      <div className="container p-2 flex flex-col w-full">
+      <div className="container mx-auto  px-12 flex flex-col w-full">
         {children}
       </div>
       <Footer/>
